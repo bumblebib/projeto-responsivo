2cbb13dcc38b0ec4b455e176845219a01e398654
Bianca Ribeiro <biancaribeiro93036@gmail.com>
2022-04-03 18:27:30 -0300

    Adiciona seções

--- js/default.js
+++ js/default.js
@@ -2,4 +2,16 @@ $( document ).ready(function() {
     $('.menu-hamburguer').on("click",function(){
         $(".conteudo-menu").toggleClass("ativo");
     })
+
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 0)
+      {
+          $('nav').addClass("ativo");
+      }
+      else
+        {
+          $('nav').removeClass("ativo");
+        }
+      });
 });
+
