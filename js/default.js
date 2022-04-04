149c47f24728540f4edd305352a99a51253779b2
Bianca Ribeiro <90845861+bumblebib@users.noreply.github.com>
2022-04-04 17:30:19 -0300

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
     })
 
     $(window).scroll(function(){
-        if($(document).scrollTop() > 50)
+        if($(document).scrollTop() > 300)
       {
           $('nav').addClass("ativo");
       }
